fb2c575baa3bd9a51d83198f4652db563f9e7065
Andrew Simbolon <2381020@unai.edu>
2024-12-05 11:54:04 +0700

    Update Main.java

--- AplikasiBarangBangunan/Main.java
+++ AplikasiBarangBangunan/Main.java
@@ -1,84 +1,38 @@
-package todoapp.views;
+package todoapp.config;
 
-import todoapp.entities.TodoList;
-import todoapp.services.TodoListService;
-import todoapp.services.TodoListServiceImpl;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-import java.util.Scanner;
+public class Database {
+    private final String dbName;
+    private final String userName;
+    private final String password;
+    private final String host;
+    private final String port;
+    private Connection connection;
 
-public class Main {
+    public Database(final String dbName, final String userName, final String password, final String host, final String port) {
+        this.dbName = dbName;
+        this.userName = userName;
+        this.password = password;
+        this.host = host;
+        this.port = port;
+    }
 
-    private static final TodoListService todoService = new TodoListServiceImpl();
-    private static final Scanner scanner = new Scanner(System.in);
+    public Connection getConnection() {
+        return connection;
+    }
 
-    public static void main(String[] args) {
-        boolean running = true;
+    public void setup() {
+        String mysqlConnUrlTemplate = "jdbc:mysql://%s:%s/%s";
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(String.format(mysqlConnUrlTemplate, host, port, dbName), userName, password);
+            System.out.println("Database connected!");
 
-        while (running) {
-            System.out.println("\n=== Aplikasi Barang Bangunan ===");
-            System.out.println("1. Lihat TodoList");
-            System.out.println("2. Tambah Barang");
-            System.out.println("3. Hapus Barang");
-            System.out.println("4. Edit Barang");
-            System.out.println("5. Keluar");
-            System.out.print("Pilih menu: ");
-
-            String choice = scanner.nextLine();
-
-            switch (choice) {
-                case "1":
-                    showTodoList();
-                    break;
-                case "2":
-                    addTodoList();
-                    break;
-                case "3":
-                    removeTodoList();
-                    break;
-                case "4":
-                    editTodoList();
-                    break;
-                case "5":
-                    running = false;
-                    System.out.println("Terima kasih telah menggunakan aplikasi!");
-                    break;
-                default:
-                    System.out.println("Pilihan tidak valid.");
-            }
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
-    }
-
-    private static void showTodoList() {
-        TodoList[] todos = todoService.getTodoList();
-        System.out.println("\nDaftar Barang:");
-        if (todos.length == 0) {
-            System.out.println("Tidak ada barang.");
-        } else {
-            for (int i = 0; i < todos.length; i++) {
-                System.out.println((i + 1) + ". " + todos[i].getTask());
-            }
-        }
-    }
-
-    private static void addTodoList() {
-        System.out.print("Masukkan nama barang: ");
-        String todo = scanner.nextLine();
-        todoService.addTodoList(todo);
-    }
-
-    private static void removeTodoList() {
-        showTodoList();
-        System.out.print("Masukkan nomor barang yang ingin dihapus: ");
-        int number = Integer.parseInt(scanner.nextLine());
-        todoService.removeTodoList(number);
-    }
-
-    private static void editTodoList() {
-        showTodoList();
-        System.out.print("Masukkan nomor barang yang ingin diedit: ");
-        int number = Integer.parseInt(scanner.nextLine());
-        System.out.print("Masukkan nama baru: ");
-        String newTodo = scanner.nextLine();
-        todoService.editTodoList(number, newTodo);
     }
 }
