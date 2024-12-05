23789535403367ff28502441c993f16fb60d06a9
Andrew Simbolon <2381020@unai.edu>
2024-12-05 11:56:54 +0700

    Update TodoListRepository.java

--- AplikasiBarangBangunan/repositories/TodoListRepository.java
+++ AplikasiBarangBangunan/repositories/TodoListRepository.java
@@ -1,12 +1,32 @@
 package todoapp.repositories;
 
-import entities.BarangBangunan;
+import todoapp.entities.BarangBangunan;
+
 import java.util.List;
 
-public interface TodoListRepository {
-    List<BarangBangunan> getAllBarang();
-    BarangBangunan getBarangById(String id);
-    boolean addBarang(BarangBangunan barang);
-    boolean updateBarang(BarangBangunan barang);
-    boolean deleteBarang(String id);
+public interface BarangBangunanRepository {
+
+    // Menyimpan barang baru
+    void save(BarangBangunan barang);
+
+    // Memperbarui barang berdasarkan ID
+    boolean update(String id, String namaBaru, Double hargaBaru);
+
+    // Menghapus barang berdasarkan ID
+    boolean delete(String id);
+
+    // Mengambil semua barang
+    List<BarangBangunan> findAll();
+
+    // Mencari barang berdasarkan ID
+    BarangBangunan findById(String id);
+
+    // Mencari barang berdasarkan nama
+    List<BarangBangunan> findByName(String name);
+
+    // Menyortir barang berdasarkan kriteria
+    List<BarangBangunan> sort(String kriteria);
+
+    // Menghitung jumlah barang yang ada
+    int count();
 }
