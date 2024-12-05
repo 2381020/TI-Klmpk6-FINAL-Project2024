51bbe6dfabc5798085129de4c7bd8c38f01095d8
Andrew Simbolon <2381020@unai.edu>
2024-12-05 11:57:46 +0700

    Update Database.java

--- AplikasiBarangBangunan/config/Database.java
+++ AplikasiBarangBangunan/config/Database.java
@@ -1,37 +1,38 @@
-package config;
+package todoapp.config;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Database {
+    private final String dbName;
+    private final String userName;
+    private final String password;
+    private final String host;
+    private final String port;
+    private Connection connection;
 
-    // URL, username, dan password database
-    private static final String URL = "jdbc:mysql://localhost:3306/todoapp?useSSL=false"; // Tambahkan parameter opsional jika perlu
-    private static final String USERNAME = "root"; // Ganti dengan username database Anda
-    private static final String PASSWORD = ""; // Ganti dengan password database Anda
-
-    // Metode untuk mendapatkan koneksi ke database
-    public static Connection getConnection() throws SQLException {
-        try {
-            // Memastikan driver JDBC tersedia
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            throw new SQLException("Driver JDBC tidak ditemukan.", e);
-        }
-
-        // Mengembalikan koneksi ke database
-        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    public Database(final String dbName, final String userName, final String password, final String host, final String port) {
+        this.dbName = dbName;
+        this.userName = userName;
+        this.password = password;
+        this.host = host;
+        this.port = port;
     }
 
-    // Metode untuk menutup koneksi (opsional, untuk memastikan koneksi tertutup)
-    public static void closeConnection(Connection connection) {
-        if (connection != null) {
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                System.err.println("Gagal menutup koneksi: " + e.getMessage());
-            }
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void setup() {
+        String mysqlConnUrlTemplate = "jdbc:mysql://%s:%s/%s";
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(String.format(mysqlConnUrlTemplate, host, port, dbName), userName, password);
+            System.out.println("Database connected!");
+
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
 }
