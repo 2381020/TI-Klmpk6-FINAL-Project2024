2232b58dd900d39cb69abfe828f7b13c6004c844
Andrew Simbolon <2381020@unai.edu>
2024-12-05 11:57:22 +0700

    Update BarangBangunan.java

--- AplikasiBarangBangunan/entities/BarangBangunan.java
+++ AplikasiBarangBangunan/entities/BarangBangunan.java
@@ -1,38 +1,49 @@
-package entities;
+package todoapp.entities;
 
 public class BarangBangunan {
     private String id;
     private String nama;
     private double harga;
 
+    // Constructor
     public BarangBangunan(String id, String nama, double harga) {
         this.id = id;
         this.nama = nama;
         this.harga = harga;
     }
 
+    // Getters and Setters
     public String getId() {
         return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getNama() {
         return nama;
     }
 
-    public double getHarga() {
-        return harga;
-    }
-
     public void setNama(String nama) {
         this.nama = nama;
+    }
+
+    public double getHarga() {
+        return harga;
     }
 
     public void setHarga(double harga) {
         this.harga = harga;
     }
 
+    // Override toString for easy display
     @Override
     public String toString() {
-        return id + " - " + nama + " - Rp " + harga;
+        return "BarangBangunan{" +
+                "id='" + id + '\'' +
+                ", nama='" + nama + '\'' +
+                ", harga=" + harga +
+                '}';
     }
 }
